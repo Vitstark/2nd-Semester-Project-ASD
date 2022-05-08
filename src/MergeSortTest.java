bc86a1dd12c7244166da5e1c10e2141e195bf223
Vitalii Chekushkin <89994306+Vitstark@users.noreply.github.com>
2022-05-08 22:23:46 +0300

    Добавил полезные методы

---
+++ src/MergeSortTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import org.junit.Assert.*;
+
+public class MergeSortTest {
+
+    public static Integer[] arrayOfIntegers = {1, 2, 3, 3, 4, 5, 7, 8, 1, 6, 6, 8, 10, 12, 12};
+    public static String []
+
+
+
+    @Test
+    public void mergeSubArrays() {
+
+    }
+}
