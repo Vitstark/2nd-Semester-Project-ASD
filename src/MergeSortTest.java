771465bca0d46789ac9226b3844ead36a6f9950c
Vitalii Chekushkin <89994306+Vitstark@users.noreply.github.com>
2022-05-08 22:36:50 +0300

    Добавил тест

--- src/MergeSortTest.java
+++ src/MergeSortTest.java
@@ -1,15 +1,16 @@
 import org.junit.*;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 public class MergeSortTest {
 
     public static Integer[] arrayOfIntegers = {1, 2, 3, 3, 4, 5, 7, 8, 1, 6, 6, 8, 10, 12, 12};
-    public static String []
-
-
+    public static String [] arrayOfStrings;
 
     @Test
     public void mergeSubArrays() {
-
+        Integer [] arrayAfterMerge = {1, 1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 8, 10, 12, 12};
+        MergeSort.merge(0, 7,
+                arrayOfIntegers.length - 1, arrayOfIntegers, (x, y) -> x - y);
+        assertEquals(arrayAfterMerge, arrayOfIntegers);
     }
 }
